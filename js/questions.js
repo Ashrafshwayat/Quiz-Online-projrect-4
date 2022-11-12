400f62761f10e0480ed1e289cf7481221fc1b8fb
Your Name <you@example.com>
2022-11-12 14:17:56 +0300

    my first commit

---
+++ js/questions.js
@@ -0,0 +1,80 @@
+let MCQS = [{
+    question: "Batool........from Korea last week",
+    
+    choice1: "A :got back",
+    choice2: "B: getted back",
+    choice3: "C: has gotten back ",
+    choice4: "d: gotten back",
+    answer: 1
+},
+            {
+            question: "The wind ______ hard all night.",
+    choice1: "A: blow",
+    choice2: "B: blown",
+    choice3: "C: blew ",
+    choice4: "D: blowing",
+    answer: 2
+},
+            {
+            question: "As the tree was too high to climb, the mischievous boys ---------- their ball down only by throwing sticks at it.",
+    choice1: " A: would have knocked",
+    choice2: "B:  were able to knock",
+    choice3: "C: will have to knock",
+    choice4: "D: may have knocked",
+    answer: 1
+},
+            {
+            question: "She failed to get anyone to repair the taps for her at the weekend, -------- could she do it on her own.",
+    choice1: "A) nor",
+    choice2: "B) as",
+    choice3: "C) only if",
+    choice4: "E) though",
+    answer: 0
+},
+            {
+            question: "If I -------- the choice of making either an oral or a written report, I -------- the second alternative.",
+    choice1: "A) had been given / might pick  ",
+    choice2: "B) were given / would pick",
+    choice3: "C) would have given / had picked",
+    choice4: "D) have been giving / will pick",
+    answer: 1
+},
+            {
+            question: "You would not expect anyone -------- intelligent to make ------ stupid mistake, but he did so.",
+    choice1: "A) too / such",
+    choice2: "B) so / that",
+    choice3: "C) as / as",
+    choice4: "D) more / than",
+    answer: 2
+},
+            {
+               question: "-------- a celebrity arrives in Istanbul, the first question reporters ask them is ------- they like Istanbul.",
+    choice1: "A) During / whether",
+    choice2: "B) Every time / how ",
+    choice3: "c) so / that",
+    choice4: "D) so / thatso / that ",
+    answer: 3
+},{
+               question: "Despite my insistence, Ashraf didn’t tell me -------- he didn’t like me and my family.",
+    choice1: "A) even if",
+    choice2: "B) what",
+    choice3: "C) why",
+    choice4: "D) the reason",
+    answer: 2
+},
+            {
+               question: "Tourists -------- when large numbers of middle-class people -------- to join the more wealthy aristocratic travellers.",
+    choice1: "A) have originated / had begun",
+    choice2: "B) could have originated / have begun ",
+    choice3: "C) could have originated / have begun  originated / began",
+    choice4: "D) were originating / are beginning",
+    answer: 2
+},
+            {
+               question: "What are you wearing to the party?",
+    choice1: "A) We're wearing suits.",
+    choice2: "B)  Wear wearing shirts.",
+    choice3: "C) will have lain ",
+    choice4: "D)  I are wearing a suits.",
+    answer: 0
+}];
