77523dff3faf64aca89bd661b5a8378db097a8a3
Your Name <you@example.com>
2022-11-15 09:19:17 +0300

    Upload

---
+++ Nouran/welcome.js
@@ -0,0 +1,15 @@
+var y = sessionStorage.getItem("username");
+
+console.log(y);
+
+document.getElementById("username").innerHTML = y;
+
+
+
+function logout() {
+
+    location.href = "../Batool/project3.html";
+   
+    sessionStorage.clear();
+
+}
